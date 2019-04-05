9492cb00916b10f3b4f31958bcf5acf44fb6eaf2
amdgit <rajendrajadeja@yahoo.com>
2019-04-06 08:52:28 +1100

    t4 modified

--- t4_video-4.js
+++ t4_video-4.js
@@ -2,10 +2,13 @@ var myapp = new Vue({
 	el:"#my-app",
 	data:{
 		name:"Online Web Tutors",
-		ulink :"https://www.youtube.com",
-		utclass:"utclass",
+		ulink :"https://www.youtube.com/watch?v=BqCaoGuRDC4&list=PLT9miexWCpPUZ-xD9s70PhhP37blPSuax&index=4",
+		utclass:"utlink",
 		imageLink :"https://picsum.photos/900/300/?random",
-		isActive:true
+		isActive:true,
+		txtName:"Raj",
+		txtEmail:"asdf@example.com",
+		htmlContent :"<p>This paragraph set from js file</p>"
 	},
 	methods :{
 	}
