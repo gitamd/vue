f6e07e430280a9b2cd895101a917db0d30b1d80c
gitamd <jadeja007@gmail.com>
2019-04-06 08:23:11 +1100

    t4 added

---
+++ t4_video-4.js
@@ -0,0 +1,13 @@
+var myapp = new Vue({
+	el:"#my-app",
+	data:{
+		name:"Online Web Tutors",
+		ulink :"https://www.youtube.com",
+		utclass:"utclass",
+		imageLink :"https://picsum.photos/900/300/?random",
+		isActive:true
+	},
+	methods :{
+	}
+
+});
